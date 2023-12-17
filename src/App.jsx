70289f917a45a9318bd6690449c050b7531e2ccd
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2023-12-17 13:44:32 +0530

    Dynamic Routes for every product in products

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/Home"
 import ProductsPage from "./pages/Products"
 import RouteLayout from "./components/RouteLayout"
 import ErrorPage from "./pages/Error"
+import ProductsDetails from "./pages/ProductsDetail"
 
 // **** Prefered ****
 const router = createBrowserRouter([
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
 
     {path:'/',element:<HomePage></HomePage>},
     {path:'/products',element:<ProductsPage></ProductsPage>},
+    {path:'/products/:productId',element:<ProductsDetails></ProductsDetails>}
   ]}
   // {path:'',},
 ])
