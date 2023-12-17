b97d3725f775da582e609a029765def72aa43ebe
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2023-12-17 13:31:29 +0530

    navigate programatically

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = ()=>{
+    const navigate = useNavigate()
+    const navigateHandler = ()=>{
+        navigate('/products')
+    }
     return(
         <div  className="">
         <div className="text-5xl">Home Page</div>
         {/* <p>
             Goto <Link to='/products'>The List of Products</Link>
             </p> */}
+        <button  className='text-2xl bg-black text-white px-4 py-2 my-8' onClick={navigateHandler}>Navigate</button>
         </div>
     )
 }
