a5237e0c8a3ab8f1f731cc5f84f85b293f40cb09
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2023-12-17 13:26:11 +0530

    Show Link Active with underline and text color"

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom"
-
+import { Link, NavLink } from "react-router-dom"
+import classes from './MainNavigation.module.scss'
 const MainNavigation = ()=>{
     return (
         <header className={` bg-slate-800  min-w-5xl m-auto p-8 flex justify-center text-2xl`}>
             <nav className="">
-                <ul className="flex gap-8 ">
+                <ul className={`flex gap-8 ${classes.list}`}>
                     <li className=" decoration-solid  text-green-600">
-                        <Link to='/'>Home</Link>
+                    <NavLink to='/' className={({isActive})=>isActive?classes.active:undefined} end>Home</NavLink>
                     </li>
                     <li className=" decoration-solid  text-green-600">
-                        <Link to='/products'>Products</Link>
+                        <NavLink to='/products' className={({isActive})=>isActive?'text-white underline':undefined}>Products</NavLink>
                     </li>
                 </ul>
             </nav>
