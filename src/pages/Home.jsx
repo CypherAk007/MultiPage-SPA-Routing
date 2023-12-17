17eae1f8ab9df35432bf223543ba43dcf7bd1aea
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2023-12-17 12:08:56 +0530

    how to navigate b/w pages using LINK

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
+import { Link } from "react-router-dom";
+
 const HomePage = ()=>{
     return(
+        <>
         <div>Home Page</div>
+        <p>
+            Goto <Link to='/products'>The List of Products</Link>
+            </p>
+        </>
     )
 }
 
